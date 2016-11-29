aa886283218e8cef35b8381d6a291308cb773995
Raj Chaudhuri <rajch@hotmail.com>
2016-11-30 02:39:14 +0530

    Wrap Docker API using dockerode. First try.

--- server.js
+++ server.js
@@ -2,11 +2,28 @@ var express = require('express');
 var morgan = require('morgan');
 var app = express();
 
+var Docker = require('dockerode');
+var docker = new Docker();
+
 // Log requests
 app.use(morgan('dev'));
 
 // Read static files from public subdirectory
 app.use(express.static('public'));
+
+
+app.get('/containers/json', function(req, res){
+  docker.listContainers(function(err,containers){
+    res.json(containers);
+  });
+});
+
+app.get('/images/json', function(req, res){
+  docker.listImages(function(err,images){
+    res.json(images);
+  });
+});
+
 
 // Listen on port 8080 by default
 app.listen(8080, function () {

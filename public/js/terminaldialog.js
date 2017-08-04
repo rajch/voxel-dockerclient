98b33973803f61dcad6c4bb015716cd58417bc1b
Raj Chaudhuri <rajch@hotmail.com>
2017-08-04 21:17:21 +0530

    Changed terminaldialog.js because docker attach uses binary websockets now. To be made more efficient.

--- public/js/terminaldialog.js
+++ public/js/terminaldialog.js
@@ -9,13 +9,12 @@
 
   window.addEventListener('message', onMessageReceived, false);
 
-  function onMessageReceived(event)
-  {
+  function onMessageReceived(event) {
     sourceWindow = event.source;
     eventOrigin = event.origin;
 
-    if(event.data.message === 'init') {
-      if(!window.WebSocket) {
+    if (event.data.message === 'init') {
+      if (!window.WebSocket) {
         // Websocket support not present.
       } else {
         // Game will send us container name
@@ -25,33 +24,49 @@
         terminal.open(document.getElementById('terminal-container'));
 
         var wsUri = "ws://" + window.location.host + "/websocket/containers/" + containername +
-                    "/attach/ws?logs=0&stderr=1&stdout=1&stream=1&stdin=1";
+          "/attach/ws?logs=0&stderr=1&stdout=1&stream=1&stdin=1";
 
         socket = new WebSocket(wsUri);
+        socket.binaryType = "arraybuffer"
+
+
         socket.onopen = function onOpen(evt) {
           terminal.writeln("Session started. If you don't see a prompt, press enter or/and Ctrl+L");
         };
 
         socket.onclose = function onClose(evt) {
           terminal.writeln("Session terminated");
-          if(!errorcreated)
-            sendMessage({ message : 'cancel' });
+          if (!errorcreated)
+            sendMessage({ message: 'cancel' });
         };
 
-        socket.onmessage = function onMessage(evt) { terminal.write(evt.data); };
+        function ab2str(buf) {
+          return String.fromCharCode.apply(null, new Uint8Array(buf));
+        }
+
+        socket.onmessage = function onMessage(evt) {
+          if (evt.data instanceof ArrayBuffer) {
+            terminal.write(ab2str(evt.data))
+          } else {
+            terminal.write(evt.data);
+          }
+        };
+
         socket.onerror = function onError(evt) {
           terminal.writeln('ERROR:' + JSON.stringify(evt));
           errorcreated = true;
         };
 
-        terminal.on('data', function(data) { socket.send(data); });
+        terminal.on('data', function (data) {
+          socket.send(data);
+        });
+        
       }
     }
   }
 
-  function sendMessage(message)
-  {
-    if(sourceWindow) {
+  function sendMessage(message) {
+    if (sourceWindow) {
       sourceWindow.postMessage(message, eventOrigin);
     }
   }

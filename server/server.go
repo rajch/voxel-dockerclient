c9cf298004b35f29b32d8402d477d5b2dc9a9e0c
Raj Chaudhuri <rajch@hotmail.com>
2017-08-05 19:28:45 +0530

    Created a simple web server and reverse proxy (for docker api) in go. Really small docker image.

---
+++ server/server.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+func main() {
+	// Verify presence of docker socket
+	conn, err := net.Dial("unix", "/var/run/docker.sock")
+	if err != nil {
+		log.Fatalln("Could not connect to docker socket. Please map it using -v /var/run/docker.sock:/var/run/docker.sock")
+	}
+	conn.Close()
+
+	socketURL, _ := url.Parse("http://unix/v1.27")
+
+	// Set up reverse proxy for docker api, using httputl.Reverproxy
+	// combined with a unix socket transport
+	revProxy := httputil.NewSingleHostReverseProxy(socketURL)
+	(*revProxy).Transport = &http.Transport{
+		DialContext: func(_ context.Context, _, _ string) (net.Conn, error) {
+			return net.Dial("unix", "/var/run/docker.sock")
+		}}
+
+	// Set up reverse proxy for docker api websockets, using code adapted from
+	// https://groups.google.com/forum/#!topic/golang-nuts/KBx9pDlvFOc
+	// Second post, by bradfitz. Many thanks.
+	revWSProxy := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		d, err := net.Dial("unix", "/var/run/docker.sock")
+		if err != nil {
+			http.Error(w, "Error contacting backend server.", 500)
+			log.Printf("WebSocket Proxy: Error dialing docker socket: %v", err)
+			return
+		}
+
+		hj, ok := w.(http.Hijacker)
+		if !ok {
+			http.Error(w, "Not a hijacker?", 500)
+			return
+		}
+
+		nc, _, err := hj.Hijack()
+		if err != nil {
+			log.Printf("WebSocket Proxy: Hijack error: %v", err)
+			return
+		}
+
+		defer nc.Close()
+		defer d.Close()
+
+		err = r.Write(d)
+		if err != nil {
+			log.Printf("WebSocket Proxy: Error copying request to target: %v", err)
+			return
+		}
+
+		errc := make(chan error, 2)
+		cp := func(dst io.Writer, src io.Reader) {
+			_, err := io.Copy(dst, src)
+			errc <- err
+		}
+		go cp(d, nc)
+		go cp(nc, d)
+		<-errc
+	})
+
+	http.Handle("/api/", http.StripPrefix("/api/", revProxy))
+
+	// Hook the path /websocket to the docker api websockets
+	// Keeping websockets separate simplifies the proxying
+	http.Handle("/websocket/", http.StripPrefix("/websocket", revWSProxy))
+
+	http.Handle("/", http.FileServer(http.Dir("../public")))
+
+	log.Println("Server starting...")
+	if err := http.ListenAndServe(":80", nil); err != nil {
+		log.Fatalf("The server failed with the following error:%v\n", err)
+	}
+
+}
